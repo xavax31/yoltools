27c25f4e36ebdb10ae38eb605688378243686d53
xb <xavier.boisnon@gmail.com>
2020-06-13 14:34:27 +0200

    fix publish script

--- publish.js
+++ publish.js
@@ -27,7 +27,7 @@ prompt.get(schema, function (err, result) {
 
     execSync("git add .", {stdio:[0, 1, 2]});
     // execSync("git commit -m '" + result.commitMessage + "'", {stdio:[0, 1, 2]});
-    execSync("npm version  " + result.version + "  -m '" + result.commitMessage + "'", {stdio:[0, 1, 2]});
+    execSync("npm version  " + result.version + "  -m '" + result.commitMessage + "' --force", {stdio:[0, 1, 2]});
     execSync("git push --tags", {stdio:[0, 1, 2]});
     execSync("npm publish", {stdio:[0, 1, 2]});
 });
