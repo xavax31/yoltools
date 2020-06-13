f0e17fd9c14ff6fabd1166ef9b3f621d3ca68e22
xb <xavier.boisnon@gmail.com>
2020-06-13 14:38:27 +0200

    add gitac script

---
+++ gitac.js
@@ -0,0 +1,21 @@
+var prompt = require('prompt');
+const execSync = require('child_process').execSync;
+
+
+var schema = {
+    properties: {
+      commitMessage: {
+        description: `Commit message`,
+        required: true
+      },
+    }
+  };
+
+prompt.start(); // Start the prompt
+
+prompt.get(schema, function (err, result) {
+    console.log('Command-line input received:', result);
+
+    execSync("git add .", {stdio:[0, 1, 2]});
+    execSync("git commit -m '" + result.commitMessage + "'", {stdio:[0, 1, 2]});
+});
